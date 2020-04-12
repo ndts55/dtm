dbadbbb95a4773d085b3f41e313a73a710def491
ndts55 <nicolastorres@mailbox.org>
2020-04-12 19:39:09 +0200

    added some tests for tape methods

--- src/tape/tests.rs
+++ src/tape/tests.rs
@@ -31,21 +31,84 @@ mod read_tests {
 }
 
 #[cfg(test)]
-mod read_at_tests {
+mod write_tests {
     #[test]
-    fn normal_behaviour() {
-        let tape = super::standard_tape();
-        for e in -4..=3 {
-            assert_eq!(Some(e), tape.read_at(e));
+    fn at_valid_index() {
+        let mut tape = super::standard_tape();
+
+        for (i, e) in (-4..=3).enumerate() {
+            tape.index = e;
+            tape.write(Some(i as isize));
         }
+
+        assert_eq!(
+            vec![3, 2, 1, 0]
+                .into_iter()
+                .map(Some)
+                .collect::<Vec<Option<isize>>>(),
+            tape.negative
+        );
+
+        assert_eq!(
+            vec![4, 5, 6, 7]
+                .into_iter()
+                .map(Some)
+                .collect::<Vec<Option<isize>>>(),
+            tape.positive
+        );
     }
 
     #[test]
-    fn out_of_bounds_behaviour() {
-        let tape = super::standard_tape();
-        assert_eq!(tape.read_at(3), Some(3));
-        assert_eq!(tape.read_at(4), None);
-        assert_eq!(tape.read_at(-4), Some(-4));
-        assert_eq!(tape.read_at(-5), None);
+    fn at_invalid_index() {
+        let mut tape = super::standard_tape();
+
+        tape.index = -5;
+        tape.write(Some(-5));
+        tape.index = 4;
+        tape.write(Some(4));
+
+        assert_eq!(
+            vec![-1, -2, -3, -4, -5]
+                .into_iter()
+                .map(Some)
+                .collect::<Vec<Option<isize>>>(),
+            tape.negative
+        );
+
+        assert_eq!(
+            vec![0, 1, 2, 3, 4]
+                .into_iter()
+                .map(Some)
+                .collect::<Vec<Option<isize>>>(),
+            tape.positive
+        );
+    }
+}
+
+#[cfg(test)]
+mod shift_tests {
+    use super::super::Direction::*;
+    #[test]
+    fn left() {
+        let mut tape = super::standard_tape();
+        assert_eq!(tape.index, 0);
+        tape.shift(Left);
+        assert_eq!(tape.index, -1);
+    }
+
+    #[test]
+    fn right() {
+        let mut tape = super::standard_tape();
+        assert_eq!(tape.index, 0);
+        tape.shift(Right);
+        assert_eq!(tape.index, 1);
+    }
+
+    #[test]
+    fn stand() {
+        let mut tape = super::standard_tape();
+        assert_eq!(tape.index, 0);
+        tape.shift(Stand);
+        assert_eq!(tape.index, 0);
     }
 }
